0cd803b818a8adb6f37aeb4c3bcaf2d055a5f273
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-06-09 16:27:40 +0700

    feat : add router in app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import FormData from './pages/FormData';
+import ListData from './pages/ListData';
+import PreviewData from './pages/PreviewData';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<ListData/>}/>
+            <Route path='/add' element={<FormData/>}/>
+            <Route path='/preview-data' element={<PreviewData/>}/>
+         </Routes>    
+      </BrowserRouter>
+   );
 }
 
 export default App;
